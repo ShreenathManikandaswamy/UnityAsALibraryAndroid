eab63097e2e194eb50a713036ff19ddf59b239c2
Shreenath M <shreenath585@gmail.com>
2023-03-23 15:06:44 +0530

    Unity setup

---
+++ UnityLibrary/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    private void Start()
+    {
+        text.text = "Opening unity content";
+    }
+
+    private void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+            if (Input.GetKeyDown(KeyCode.Escape)) 
+                Application.Quit();
+    }
+}
